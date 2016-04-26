1e9659e31f3088aab087ab52633b7b9cfd51f4ab
William Benton <willb@redhat.com>
2016-04-26 12:17:15 -0500

    Combine immutable stream estimates

--- src/main/scala/com/freevariable/capriccio/immutable/sink.scala
+++ src/main/scala/com/freevariable/capriccio/immutable/sink.scala
@@ -22,22 +22,41 @@ package com.freevariable.capriccio.immutable
 
 /**
  * On-line mean and variance estimates for a stream of Double values.
- * Uses the technique from <a href="http://dl.acm.org/citation.cfm?id=359153">"Updating mean and variance estimates: an improved method"</a>, by D. H. D. West (1979).
-*/
-sealed case class Sink(val count: Long, val min: Double, val max: Double, val mean: Double, val variance: Double, val sumX2: Double) {
+ * Supports combining estimates of multiple streams.  Uses Chan's technique
+ * (later refined by Terriberry and Pébay).
+ */
+sealed case class Sink(val count: Long, val min: Double, val max: Double, val mean: Double, val m2: Double) {
   // TODO:  parameterize this over sample (at least), fractional (mean/variance), and integral (count) types
   
-  // TODO:  rework to use Pébay's algorithm (support arbitrary moments, support combining stream estimates) to make this code more generally useful
+  // TODO:  use Pébay's formulae (supporting arbitrary moments) to make this code more generally useful
 
   @inline def put(sample: Double) = {
-    val dev = sample - mean
-    val newMean = mean + (dev / (count + 1))
-    val newSumX2 = sumX2 + (dev * (sample - newMean))
-    this.copy(count=count + 1, min=math.min(min, sample), max=math.max(max, sample), mean=newMean, sumX2=newSumX2, variance=(newSumX2 / (count + 1)))
+    val dev = (sample - mean)
+    val newCount = count + 1
+    val newMean = mean + (dev / (newCount))
+    val newM2 = m2 + (dev * dev) * count / newCount
+    
+    this.copy(count=newCount, min=math.min(min, sample), max=math.max(max, sample), mean=newMean, m2=newM2)
   }
+  
+  def ++(other: Sink) = {
+    (this, other) match {
+      case (empty, _) if empty.count == 0L => other
+      case (_, empty) if empty.count == 0L => this
+      case _ => {
+	val dev = other.mean - mean
+	val newCount = this.count + other.count
+	val newMean = (this.count * this.mean + other.count * other.mean) / newCount
+	val newM2 = this.m2 + other.m2 + (dev * dev) * this.count * other.count / newCount
+	this.copy(count=newCount, min=math.min(this.min, other.min), max=math.max(this.max, other.max), mean=newMean, m2=newM2)
+      }
+    }
+  }
+
+  val variance = m2 / count
   def stddev = math.sqrt(variance)
 }
 
 object Sink {
-  def empty: Sink = Sink(0, Double.PositiveInfinity, Double.NegativeInfinity, 0.0d, 0.0d, 0.0d)
+  def empty: Sink = Sink(0, Double.PositiveInfinity, Double.NegativeInfinity, 0.0d, 0.0d)
 }
