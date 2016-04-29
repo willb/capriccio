e8430adcb43cf8966ce4ac68f11ca916a87f3518
William Benton <willb@redhat.com>
2016-04-29 09:57:40 -0500

    destructive and nondestructive combine for mutable sinks

--- src/main/scala/com/freevariable/capriccio/mutable/sink.scala
+++ src/main/scala/com/freevariable/capriccio/mutable/sink.scala
@@ -24,7 +24,7 @@ package com.freevariable.capriccio.mutable
  * On-line mean and variance estimates for a stream of Double values.
  * Uses the technique from <a href="http://dl.acm.org/citation.cfm?id=359153">"Updating mean and variance estimates: an improved method"</a>, by D. H. D. West (1979).
 */
-sealed class Sink(private var _count: Long, private var _min: Double, private var _max: Double, private var _mean: Double, private var _variance: Double, private var _sumX2: Double) {
+sealed class Sink(private var _count: Long, private var _min: Double, private var _max: Double, private var _mean: Double, private var _m2: Double) {
   // TODO:  parameterize this over sample (at least), fractional (mean/variance), and integral (count) types
   
   // TODO:  rework to use Pébay's algorithm (support arbitrary moments, support combining stream estimates) to make this code more generally useful
@@ -32,18 +32,47 @@ sealed class Sink(private var _count: Long, private var _min: Double, private va
   @inline def put(sample: Double) = {
     val dev = sample - _mean
     _mean = _mean + (dev / (count + 1))
-    _sumX2 = _sumX2 + (dev * (sample - _mean))
+    _m2 = _m2 + (dev * dev) * count / (count + 1)
     _count = count + 1
     _min = math.min(_min, sample)
     _max = math.max(_max, sample)
-    _variance = _sumX2 / _count
   }
   
+  def ++(other: Sink): Sink = {
+    val result = Sink.empty += this
+    result += other
+  }
+
+  def +=(other: Sink): Sink = {
+    if(other.count == 0L) {
+      this
+    } else if (this.count == 0L) {
+      this._mean = other.mean
+      this._count = other.count
+      this._min = other.min
+      this._max = other.max
+      this._m2 = other._m2
+      this
+    } else {
+      val dev = other.mean - mean
+      val newCount = other.count + count
+      val newMean = (this.count * this.mean + other.count * other.mean) / newCount
+      val newM2 = this._m2 + other._m2 + (dev * dev) * this.count * other.count / newCount
+      this._count = newCount
+      this._min = math.min(this.min, other.min)
+      this._max = math.max(this.max, other.max)
+      this._mean = newMean
+      this._m2 = newM2
+
+      this
+    }
+  }
+
   @inline def mean = _mean
   @inline def count = _count
   @inline def min = _min
   @inline def max = _max
-  @inline def variance = _variance
+  @inline def variance = _m2 / count
   
   def stddev = math.sqrt(variance)
   
@@ -51,5 +80,5 @@ sealed class Sink(private var _count: Long, private var _min: Double, private va
 }
 
 object Sink {
-  def empty: Sink = new Sink(0, Double.PositiveInfinity, Double.NegativeInfinity, 0.0d, 0.0d, 0.0d)
+  def empty: Sink = new Sink(0, Double.PositiveInfinity, Double.NegativeInfinity, 0.0d, 0.0d)
 }
